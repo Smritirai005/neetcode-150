10b02440569c29b7347c7dc2d7cf4b14afaeb428
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2025-08-27 01:30:40 +0530

    Time: 3 ms (38.7%), Space: 17 MB (85.34%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -4,8 +4,10 @@ public:
         stack<int>st;
         for(int i=0;i<tokens.size();i++){
             if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/")          {
-                int n=st.top();st.pop();
-                int m=st.top();st.pop();
+                int n=st.top();
+                st.pop();
+                int m=st.top();
+                st.pop();
                 if(tokens[i]=="+")st.push(n+m);
                 else if(tokens[i]=="-")st.push(m-n);
                 else if(tokens[i]=="*")st.push(n*m);
