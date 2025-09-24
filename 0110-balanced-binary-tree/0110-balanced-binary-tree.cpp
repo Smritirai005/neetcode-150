dba07ec29bc0630bcbe1a11533cf44e07e67f473
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2025-09-24 10:51:02 +0530

    Time: 0 ms (100%), Space: 23.2 MB (23.33%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,20 +14,15 @@ public:
     int height(TreeNode* root){
         if(root==nullptr)return 0;
         int left=height(root->left);
+        if(left==-1)return -1;
         int right=height(root->right);
+        if(right==-1)return -1;
+        if(abs(left-right)>1){
+            return -1;
+        }
         return max(left,right)+1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr)return true;
-        int lefth=height(root->left);
-        int righth=height(root->right);
-
-        if(abs(lefth-righth)>1){
-            return false;
-        }
-
-        return isBalanced(root->left)&&isBalanced(root->right);
-
-        
+        return height(root)!=-1;
     }
 };
