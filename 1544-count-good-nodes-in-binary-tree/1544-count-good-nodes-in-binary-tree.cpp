c6117faba43ce77c7d0647eb967a5a7d5e5f4676
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2025-09-30 01:18:53 +0530

    Time: 104 ms (33.64%), Space: 92.2 MB (5.54%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -11,20 +11,30 @@
  */
 class Solution {
 public:
-    int count=0;
-    void preOrder(TreeNode* root,int mx){
-        if(root==nullptr)return;
-        if(root->val >= mx){
-            mx=root->val;
-            count++;
-        }
-        preOrder(root->left,mx);
-        preOrder(root->right,mx);
-
-    }
-
     int goodNodes(TreeNode* root) {
-        preOrder(root,root->val);
+        if (!root) return 0;
+
+        int count = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, root->val});
+
+        while (!q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                auto [node, mx] = q.front();
+                q.pop();
+
+                if (node->val >= mx){
+                    mx=node->val;
+                    count++;
+                }
+
+                if (node->left) 
+                    q.push({node->left,mx});
+                if (node->right) 
+                    q.push({node->right,mx});
+            }
+        }
         return count;
         
     }
