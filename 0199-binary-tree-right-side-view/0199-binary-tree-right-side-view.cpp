2360166e417e4d380972d07dce26528358149b97
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2025-09-30 00:25:55 +0530

    Time: 0 ms (100%), Space: 15 MB (60.76%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -18,18 +18,20 @@ public:
         q.push(root);
         while(!q.empty()){
             int n=q.size();
+            TreeNode* node=nullptr;
             for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
+                node=q.front();
                 q.pop();
-                if(i==n-1) {
-                    ans.push_back(node->val);
-                }
+                //if(i==n-1) {
+                    //ans.push_back(node->val);
+                //}
                 if(node->left)q.push(node->left);
                 if(node->right)q.push(node->right);
                 
 
 
             }
+            ans.push_back(node->val);
 
         }
         return ans;
