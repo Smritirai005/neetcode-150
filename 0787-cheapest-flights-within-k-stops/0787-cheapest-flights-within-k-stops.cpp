c3e1251c16af4104d759b9c974cde569c9c66a16
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-01-22 01:22:41 +0530

    Time: 3 ms (65.19%), Space: 17.7 MB (74.49%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& f : flights) adj[f[0]].push_back({f[1], f[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto &f : flights) {
+            adj[f[0]].push_back({f[1], f[2]});
+        }
 
         vector<int> minCost(n, INT_MAX);
         minCost[src] = 0;
 
-        queue<pair<int, pair<int, int>>> q; // {stops, {node, cost}}
-        q.push({0, {src, 0}});
+        queue<pair<int,int>> q; // {node, cost}
+        q.push({src, 0});
 
-        while (!q.empty()) {
-            auto cur = q.front(); q.pop();
-            int stops = cur.first;
-            int u = cur.second.first;
-            int cost = cur.second.second;
+        int stops = 0;
 
-            if (stops > k) continue;
+        while (!q.empty() && stops <= k) {
+            int sz = q.size();
+            while (sz--) {
+                auto [u, cost] = q.front();
+                q.pop();
 
-            for (auto& edge : adj[u]) {
-                int v = edge.first;
-                int price = edge.second;
-                if (cost + price < minCost[v]) {
-                    minCost[v] = cost + price;
-                    q.push({stops + 1, {v, minCost[v]}});
+                for (auto &[v, price] : adj[u]) {
+                    if (cost + price < minCost[v]) {
+                        minCost[v] = cost + price;
+                        q.push({v, minCost[v]});
+                    }
                 }
             }
+            stops++;
         }
+
         return minCost[dst] == INT_MAX ? -1 : minCost[dst];
     }
 };
+
