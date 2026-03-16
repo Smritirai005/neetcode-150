2e5ba4cfd7ce1e2b8e357680ed0cdd07c76edbc6
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-03-16 19:36:47 +0530

    Time: 1 ms (18.98%), Space: 9.2 MB (58.6%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    unordered_map<char,string>f;
+    void fun(string& s,int n, vector<string>&ans,string res,int idx){
+        
+        if(idx==n){
+            ans.push_back(res);
+            return;
+        }
+        string t=f[s[idx]];
+        for(int i=0;i<t.length();i++){
+            res.push_back(t[i]);
+            fun(s,n,ans,res,idx+1);
+            res.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        if(digits.size()==0) return ans;
+        f['2']="abc";
+        f['3']="def";
+        f['4']="ghi";
+        f['5']="jkl";
+        f['6']="mno";
+        f['7']="pqrs";
+        f['8']="tuv";
+        f['9']="wxyz";
+        string res="";
+        fun(digits,digits.size(),ans,res,0);
+
+        return ans;
+
+
+        
+        
+    }
+};
