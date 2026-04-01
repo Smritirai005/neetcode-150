82ea03a4cfdbbc393761504ed45e45d3c5266315
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-04-02 00:43:27 +0530

    Time: 5 ms (68.24%), Space: 12.7 MB (38.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -10,6 +10,9 @@ public:
             mp[s[j]]++;
             while(mp[s[j]]>1){
                 mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
                 i++;
 
             }
