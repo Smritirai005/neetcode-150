8f9b9dda1c2242bd1d96f53f37d1f5dd9f75af1b
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-04-03 02:14:49 +0530

    Time: 9 ms (51.95%), Space: 11.3 MB (81.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>mp1;
-        int i=0;
-        int j=0;
-        int start=0;
-        int count=t.size();;
-        int ans=INT_MAX; 
-        for(i=0;i<t.length();i++){
-            mp1[t[i]]++;
+        unordered_map<char, int> mp;
+        
+        for(char c : t) {
+            mp[c]++;
         }
-        i=0;
-        while(j<s.length()){
-            
-            if(mp1[s[j]]>0){
-                count--;
-            }
-            mp1[s[j]]--;
-            while(count==0){
-                if(j-i+1<ans){
-                    ans=j-i+1;
-                    start=i;
-                }
-                mp1[s[i]]++;
-                
 
-                if(mp1[s[i]]>0){
-                    count++;
+        int i = 0, j = 0;
+        int count = t.size();
+        int mn = INT_MAX, start = 0;
+
+        while(j < s.size()) {
+            if(mp[s[j]] > 0) count--;
+            mp[s[j]]--;
+
+            while(count == 0) {
+                if(j - i + 1 < mn) {
+                    mn = j - i + 1;
+                    start = i;
                 }
+
+                mp[s[i]]++;
+                if(mp[s[i]] > 0) count++;
                 i++;
             }
+
             j++;
-               
-                
+        }
 
-                
-            
-
-        } 
-        return (ans==INT_MAX)?"":s.substr(start,ans);
-        
+        return mn == INT_MAX ? "" : s.substr(start, mn);
     }
 };
