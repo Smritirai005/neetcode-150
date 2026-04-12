f2a6ac0731401d7ed902499f83dd70960c350b93
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-04-13 00:35:15 +0530

    Time: 3 ms (76.39%), Space: 20.2 MB (25.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,48 @@
 class Solution {
-public:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& visited, vector<int>& currpth) {
-        visited[node] = 1;
-        currpth[node] = 1;
-
-        for (int nbr : graph[node]) {
-            if (!visited[nbr]) {
-                int res = dfs(nbr, graph, visited, currpth);
-                if (res == false) return false;
-            } 
-            else {
-                if (currpth[nbr] == 1) {
-                    return false;
-                }
-            }
+public: bool dfs(vector<vector<int>>&adj,int node, vector<int>&vis,vector<int>&currpth){
+    vis[node]=1;
+    currpth[node]=1;
+    for(int i=0;i<adj[node].size();i++){
+        int nbr=adj[node][i];
+        if(!vis[nbr]){
+            int res=dfs(adj,nbr,vis,currpth);
+            if(res==false)return false;
         }
+        else if(currpth[nbr]==1){
+            return false;
+        }
+        
+       
 
-        currpth[node] = 0;
-        return true;
     }
-
+    currpth[node]=0;
+    return true;
+}
+public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int v = numCourses;
-        vector<vector<int>> graph(v);
-        for (auto &p : prerequisites) {
-            graph[p[1]].push_back(p[0]);
-        }
-        vector<int> visited(v), currpth(v);
+        int n=numCourses;
+        vector<vector<int>>adj(n);
+        vector<int>vis(n,0);
+        vector<int>currpth(n,0);
+        for(int i=0;i<prerequisites.size();i++){
+            vector<int>edge=prerequisites[i];
+            int src=edge[0];
+            int des=edge[1];
+            adj[des].push_back(src);
 
-        for (int i = 0; i < v; i++) {
-            if (!visited[i]) {
-                int ans = dfs(i, graph, visited, currpth);
-                if (ans == false) {
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                int ans=dfs(adj,i,vis,currpth);
+                if(ans==false){
                     return false;
                 }
             }
         }
-
         return true;
+
+
+        
+        
     }
 };
