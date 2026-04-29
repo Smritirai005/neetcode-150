205a43cbd45c366b35e1b6045e41c8598ba494b6
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-04-29 23:35:40 +0530

    Time: 3 ms (66.22%), Space: 11 MB (39.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -27,3 +27,28 @@ public:
         return mx;
     }
 };
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         vector<int> freq(26, 0);
+        
+//         int i = 0, maxFreq = 0, maxLen = 0;
+        
+//         for (int j = 0; j < s.size(); j++) {
+//             freq[s[j] - 'A']++;
+            
+//             // update max frequency
+//             maxFreq = max(maxFreq, freq[s[right] - 'A']);
+            
+//             // if window is invalid
+//             while ((j - i + 1) - maxFreq > k) {
+//                 freq[s[i] - 'A']--;
+//                 i++;
+//             }
+            
+//             maxLen = max(maxLen, j - i + 1);
+//         }
+        
+//         return maxLen;
+//     }
+// };
