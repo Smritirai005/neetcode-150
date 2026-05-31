ad0f1485f497cb7155a6be932990327b914b7808
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-06-01 02:52:41 +0530

    Time: 23 ms (19.26%), Space: 15.1 MB (29.37%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-
-        priority_queue<
-            vector<int>,
-            vector<vector<int>>,
-            greater<vector<int>>
-        > pq;
-
-        vector<vector<int>> vis(n, vector<int>(n, 0));
-
-        pq.push({grid[0][0], 0, 0});
-
-        int dirs[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
-        while (!pq.empty()) {
-            auto curr = pq.top();
+        int n=grid.size();
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        pq.push({grid[0][0],0,0});
+        dist[0][0]=grid[0][0];
+        int row[4]={-1,1,0,0};
+        int col[4]={0,0,-1,1};
+        while(!pq.empty()){
+            vector<int> p = pq.top();
             pq.pop();
-
-            int time = curr[0];
-            int x = curr[1];
-            int y = curr[2];
-
-            if (vis[x][y]) continue;
-            vis[x][y] = 1;
-
-            if (x == n - 1 && y == n - 1)
+            int time = p[0];
+            int rw = p[1];
+            int cl = p[2];
+            if(rw==n-1 && cl==n-1){
                 return time;
+            }
+            for(int i=0;i<4;i++){
+                int nr=rw+row[i];
+                int nc=cl+col[i];
+                if(nr>=0&&nr<n && nc >= 0 && nc < n){
+                    int newt=max(time,grid[nr][nc]);
 
-            for (auto &d : dirs) {
-                int nx = x + d[0];
-                int ny = y + d[1];
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n && !vis[nx][ny]) {
-                    pq.push({
-                        max(time, grid[nx][ny]),
-                        nx,
-                        ny
-                    });
+                    if(newt<dist[nr][nc]){
+                        dist[nr][nc]=newt;
+                        pq.push({newt,nr,nc});
+                    }
                 }
             }
+           
         }
-
         return -1;
+
+
+
+        
+        
     }
 };
