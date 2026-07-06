d051d799e763e28af3ebc1dc1504d7cc64f89e7d
Smriti Rai <140977626+Smritirai005@users.noreply.github.com>
2026-07-07 00:59:48 +0530

    Time: 0 ms (100%), Space: 29.5 MB (93.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        //brute
         int n = nums.size();
-        k = k % n; // in case k >= n
 
-        vector<int> ans(n);
-        for (int i = 0; i < n; i++) {
-            ans[(i + k) % n] = nums[i];
-        }
+        k %= n;
 
-        nums = ans; // copy back to original
-        //optimal
-        // int n = nums.size();
-        // k = k % n;
-
-        // reverse(nums.begin(), nums.end());
-        // reverse(nums.begin(), nums.begin() + k);
-        // reverse(nums.begin() + k, nums.end());
-    
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
